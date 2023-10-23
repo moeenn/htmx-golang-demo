a6c7d18e11f72855efefbcd83b40ac8d1f9ae300
moeenn <moeen.v8@gmail.com>
2023-10-23 18:01:53 +0500

    updated templating logic

--- pkg/templates/templates.go
+++ pkg/templates/templates.go
@@ -9,7 +9,7 @@ var tmpl *template.Template
 func Get() *template.Template {
 	var err error
 	if tmpl == nil {
-		tmpl, err = template.ParseGlob("./pkg/templates/views/*.html")
+		tmpl, err = template.ParseGlob("./pkg/templates/views/**/*.html")
 		if err != nil {
 			panic("failed to load template: " + err.Error())
 		}
