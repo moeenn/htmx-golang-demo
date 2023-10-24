bd0d83734cc42cea93c1d35e84a8cc425bc9bd92
moeenn <moeen.v8@gmail.com>
2023-10-24 15:59:12 +0500

    refactor: templates module added init function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module sandbox
 
 go 1.18
 
-require github.com/google/uuid v1.3.0 // indirect
+require github.com/google/uuid v1.3.0
--- pkg/templates/templates.go
+++ pkg/templates/templates.go
@@ -2,18 +2,38 @@ package templates
 
 import (
 	"html/template"
+	"net/http"
+	"sandbox/pkg/user"
 )
 
 var tmpl *template.Template
 
-func Get() *template.Template {
-	var err error
+func init() {
 	if tmpl == nil {
+		var err error
 		tmpl, err = template.ParseGlob("./pkg/templates/views/**/*.html")
 		if err != nil {
 			panic("failed to load template: " + err.Error())
 		}
 	}
+}
 
-	return tmpl
+type UserTableRows struct {
+	Users []user.User
+}
+
+func UserRowPartialTemplate(w http.ResponseWriter, user user.User) {
+	tmpl.ExecuteTemplate(w, "user_row.partial.html", user)
+}
+
+func UserTableRowsPartialTemplate(w http.ResponseWriter, users []user.User) {
+	tmpl.ExecuteTemplate(w, "user_table_rows.partial.html", UserTableRows{
+		Users: users,
+	})
+}
+
+func HomePageTemplate(w http.ResponseWriter, users []user.User) {
+	tmpl.ExecuteTemplate(w, "home.page.html", UserTableRows{
+		Users: users,
+	})
 }
