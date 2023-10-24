37923983ee6f33104b354762bfd70a0fc511f92b
moeenn <moeen.v8@gmail.com>
2023-10-24 13:02:45 +0500

    refactor: created typed functions for executing view templates

--- pkg/controllers/user_controller.go
+++ pkg/controllers/user_controller.go
@@ -8,20 +8,12 @@ import (
 	"sandbox/pkg/user"
 )
 
-type ListAllUsersTemplateArgs struct {
-	Users []user.User
+type AddUserRequest struct {
+	Email string `json:"email"`
 }
 
 func ListAllUsersHandler(w http.ResponseWriter, r *http.Request) {
-	args := ListAllUsersTemplateArgs{
-		Users: db.Get().UsersAll(),
-	}
-
-	templates.Get().ExecuteTemplate(w, "home.page.html", args)
-}
-
-type AddUserRequest struct {
-	Email string `json:"email"`
+	templates.HomePageTemplate(w, db.Get().UsersAll())
 }
 
 func AddUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,27 +23,17 @@ func AddUserHandler(w http.ResponseWriter, r *http.Request) {
 	user := user.New(body.Email, true)
 	db.Get().UserAdd(user)
 
-	templates.Get().ExecuteTemplate(w, "user_row.partial.html", user)
+	templates.UserRowPartialTemplate(w, user)
 }
 
 func ToggleUserStatusHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	db.Get().UserActiveToggle(id)
-
-	args := ListAllUsersTemplateArgs{
-		Users: db.Get().UsersAll(),
-	}
-
-	templates.Get().ExecuteTemplate(w, "user_table_rows.partial.html", args)
+	templates.UserTableRowsPartialTemplate(w, db.Get().UsersAll())
 }
 
 func UserRemoveHandle(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	db.Get().UserRemove(id)
-
-	args := ListAllUsersTemplateArgs{
-		Users: db.Get().UsersAll(),
-	}
-
-	templates.Get().ExecuteTemplate(w, "user_table_rows.partial.html", args)
+	templates.UserTableRowsPartialTemplate(w, db.Get().UsersAll())
 }
