3272d4e5b346c0c9d402ec1f8a23569e7d9a1876
moeenn <moeen.v8@gmail.com>
2023-10-24 17:37:11 +0500

    simplified tamplate loading logic

--- pkg/templates/templates.go
+++ pkg/templates/templates.go
@@ -2,19 +2,16 @@ package templates
 
 import (
 	"html/template"
-	"net/http"
+	"io"
 	"sandbox/pkg/user"
 )
 
 var tmpl *template.Template
 
+/* templates will be parsed once at package first import */
 func init() {
 	if tmpl == nil {
-		var err error
-		tmpl, err = template.ParseGlob("./pkg/templates/views/**/*.html")
-		if err != nil {
-			panic("failed to load template: " + err.Error())
-		}
+		tmpl = template.Must(template.ParseGlob("./pkg/templates/views/**/*.html"))
 	}
 }
 
@@ -22,17 +19,18 @@ type UserTableRows struct {
 	Users []user.User
 }
 
-func UserRowPartialTemplate(w http.ResponseWriter, user user.User) {
+/* all pages and partials will have their own typed functions for execution */
+func UserRowPartialTemplate(w io.Writer, user user.User) {
 	tmpl.ExecuteTemplate(w, "user_row.partial.html", user)
 }
 
-func UserTableRowsPartialTemplate(w http.ResponseWriter, users []user.User) {
+func UserTableRowsPartialTemplate(w io.Writer, users []user.User) {
 	tmpl.ExecuteTemplate(w, "user_table_rows.partial.html", UserTableRows{
 		Users: users,
 	})
 }
 
-func HomePageTemplate(w http.ResponseWriter, users []user.User) {
+func HomePageTemplate(w io.Writer, users []user.User) {
 	tmpl.ExecuteTemplate(w, "home.page.html", UserTableRows{
 		Users: users,
 	})
