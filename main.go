9eddb4d4863af33d6834981041fc414d333e3528
moeenn <moeen.v8@gmail.com>
2023-10-25 12:30:15 +0500

    updated templates

--- main.go
+++ main.go
@@ -7,22 +7,22 @@ import (
 )
 
 const (
-	PORT = ":3000"
+	ADDRESS = "0.0.0.0:3000"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	// register all request handlers
+	/* register all request handlers */
 	mux.HandleFunc("/", controllers.ListAllUsersHandler)
 	mux.HandleFunc("/add-user", controllers.AddUserHandler)
 	mux.HandleFunc("/toggle", controllers.ToggleUserStatusHandler)
 	mux.HandleFunc("/remove", controllers.UserRemoveHandle)
 
-	// serve static files
+	/* serve static files */
 	fs := http.FileServer(http.Dir("./public"))
 	mux.Handle("/public/", http.StripPrefix("/public", fs))
 
-	log.Printf("running on port %s\n", PORT)
-	http.ListenAndServe(PORT, mux)
+	log.Printf("running on %s\n", ADDRESS)
+	http.ListenAndServe(ADDRESS, mux)
 }
